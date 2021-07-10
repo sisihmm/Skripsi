a2f8e9fb28511d9f1a43c4ae6dc92e2c9fb94acf
sisihmm <87139802+sisihmm@users.noreply.github.com>
2021-07-10 18:36:53 +0700

    login
    login

--- app/src/main/java/com/anas/skripsi/Login.java
+++ app/src/main/java/com/anas/skripsi/Login.java
@@ -1,34 +1,112 @@
 package com.anas.skripsi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Login extends AppCompatActivity {
-
+    EditText etEmail;
+    EditText etPassword;
     TextView tvForget;
+    Button bLogin;
+    TextView tvRegis;
+
+    FirebaseAuth fAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        etEmail = findViewById(R.id.lemail);
+        etPassword = findViewById(R.id.lpassw);
+        bLogin = findViewById(R.id.blogin);
+        tvRegis = findViewById(R.id.signup);
         tvForget = findViewById(R.id.tvForget);
 
-        tvForget.setOnClickListener(new View.OnClickListener() {
+        fAuth = FirebaseAuth.getInstance();
+
+
+        bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDialog();
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    etEmail.setError("Email Kosang");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    etPassword.setError("password Kosang");
+                    return;
+                }
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+                            db.collection("users").document(task.getResult().getUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    String level = task.getResult().getString("level");
+                                    Boolean isAdmin = level.equals("1");
+                                    if (isAdmin) {
+                                        Log.d("LOGIN", "onComplete: " + task.getResult().getString("level"));
+                                        Toast.makeText(Login.this, "user Login", Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), Home.class);
+                                        intent.putExtra("level", level);
+                                        startActivity(intent);
+                                    } else {
+
+                                    }
+
+                                }
+                            });
+
+
+                        } else {
+                            Toast.makeText(Login.this, "gagal login !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+
+                });
+
+                tvForget.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openDialog();
+                    }
+                });
             }
+
+            //forget password
+            public void openDialog() {
+                forget_dialog forget_Dialog = new forget_dialog();
+                forget_Dialog.show(getSupportFragmentManager(), "Forget Password");
+            }
+
         });
     }
-    //forget password
-    public void openDialog(){
-        forget_dialog forget_Dialog = new forget_dialog();
-        forget_Dialog.show(getSupportFragmentManager(),"Forget Password");
-    }
-
 }
+
--- app/src/main/java/com/anas/skripsi/forget_dialog.java
+++ app/src/main/java/com/anas/skripsi/forget_dialog.java
@@ -2,45 +2,50 @@ package com.anas.skripsi;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
-import com.google.firebase.FirebaseApp;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class forget_dialog extends AppCompatDialogFragment {
 
     private EditText fEmail;
-
+    FirebaseAuth fAuth;
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.forget_dialog, null);
+        View view = inflater.inflate(R.layout.activity_forgetd, null);
+
+        fAuth =FirebaseAuth.getInstance();
 
         builder.setView(view)
                 .setTitle("send")
-                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
 
                     }
                 })
-                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                .setPositiveButton("Send", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i  ) {
                         if (fEmail.getText().toString().isEmpty()) {
                             fEmail.setError("email kosong");
                             return;
                         }
+                        fAuth.sendPasswordResetEmail(fEmail.getText().toString());
 
                     }
                 });
