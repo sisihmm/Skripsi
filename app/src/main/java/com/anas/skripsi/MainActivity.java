d38cf91d17bf53823b5a809616167bb3e3a8f530
sisihmm <87139802+sisihmm@users.noreply.github.com>
2021-07-10 17:52:03 +0700

    reg
    reg spls

--- app/src/main/java/com/anas/skripsi/MainActivity.java
+++ app/src/main/java/com/anas/skripsi/MainActivity.java
@@ -1,13 +1,32 @@
 package com.anas.skripsi;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar splash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        splash = findViewById(R.id.psplash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                splash.setProgress(0);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
     }
 }
--- app/src/main/java/com/anas/skripsi/Register.java
+++ app/src/main/java/com/anas/skripsi/Register.java
@@ -1,14 +1,108 @@
 package com.anas.skripsi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+    EditText etName, etEmail, etPassword, etCpassword;
+    Button bRegis;
+    TextView tvLogin;
+    FirebaseFirestore db;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etName = findViewById(R.id.etfullname);
+        etEmail = findViewById(R.id.etemail);
+        etPassword = findViewById(R.id.etpassword);
+        etCpassword = findViewById(R.id.etcpassword);
+        bRegis = findViewById(R.id.btsignup);
+        tvLogin = findViewById(R.id.tvblogin);
+
+        db = FirebaseFirestore.getInstance();
+        fAuth = FirebaseAuth.getInstance();
+
+
+        if (fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),Login.class));
+            finish();
+        }
+
+        bRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etName.getText().toString().trim();
+                String email = etEmail.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                String cpassword = etCpassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(name)){
+                    etName.setError("nama kosong");
+                    return;
+                }
+                if (TextUtils.isEmpty(email)){
+                    etEmail.setError("nama email");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    etPassword.setError("nama password");
+                    return;
+                }
+                if (!password.equals(cpassword)){
+                    etCpassword.setError("nama kosong");
+                    return;
+                }
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("name", name);
+                            user.put("email", email);
+                            user.put("level", "1");
+
+                            db.collection("users").document(task.getResult().getUser().getUid()).set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(Register.this,"User Createed",Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(getApplicationContext(),Login.class));
+                                }
+                            });
+                        }else{
+                            Toast.makeText(Register.this,"User Failed Regist", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+            tvLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Register.this, Login.class);
+                        startActivity(intent);
+                }
+            });
+
     }
 }
