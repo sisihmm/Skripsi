f6607e736b6799c6acf0ac4ee6c40f41f56f3b5d
sisihmm <87139802+sisihmm@users.noreply.github.com>
2021-07-11 21:25:31 +0700

    fragment
    fragment

--- app/src/main/java/com/anas/skripsi/Home.java
+++ app/src/main/java/com/anas/skripsi/Home.java
@@ -4,7 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -17,12 +21,42 @@ import com.google.android.material.navigation.NavigationView;
 
 public class Home extends AppCompatActivity {
 
+    private static final String TAG = Home.class.getSimpleName();
+    private BottomNavigationView bottomNavigation;
+    private FragmentManager fragmentManager;
+    Fragment select;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        bottomNavigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigation.inflateMenu(R.menu.bottom_nav_menu);
+        fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction().replace(R.id.main_container, new HomeFragment()).commit();
+
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.home:
+                        select = new HomeFragment();
+                        break;
+                    case R.id.person:
+                        select = new AccountFragment();
+                        break;
+                    case R.id.settings:
+                        select = new HelpFragment();
+                        break;
+                }
+                final FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.main_container,select ).commit();
+                return true;
+            }
+        });
     }
 
 }
