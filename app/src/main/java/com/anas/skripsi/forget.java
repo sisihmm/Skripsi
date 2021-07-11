a0b2593559994955cd98a0bdca32fc1c8a3df8c1
sisihmm <87139802+sisihmm@users.noreply.github.com>
2021-07-11 19:53:22 +0700

    forget clas
    cla

--- app/src/main/java/com/anas/skripsi/forget.java
+++ app/src/main/java/com/anas/skripsi/forget.java
@@ -1,14 +1,67 @@
 package com.anas.skripsi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class forget extends AppCompatActivity {
+    TextView tvFemail;
+    EditText etFmail;
+    Button bSend;
+
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget);
+
+        tvFemail = findViewById(R.id.tvblogin);
+        etFmail = findViewById(R.id.femail);
+        bSend = findViewById(R.id.bsend);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        tvFemail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(forget.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        bSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(etFmail.getText().toString().trim())){
+                    etFmail.setError("kosong");
+                }
+
+                fAuth.sendPasswordResetEmail(etFmail.getText().toString().trim()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(forget.this,"send email succes", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(forget.this,"email failed send", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
     }
 }
