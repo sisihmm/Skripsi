61e925497eb8f25784a50fe466f488ab2566a038
sisihmm <87139802+sisihmm@users.noreply.github.com>
2021-07-17 15:01:45 +0700

    awokwaok
    apasi

--- app/src/main/java/com/anas/skripsi/user/lessonAlfabet.java
+++ app/src/main/java/com/anas/skripsi/user/lessonAlfabet.java
@@ -1,16 +1,29 @@
 package com.anas.skripsi.user;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.anas.skripsi.R;
 
+import java.util.ArrayList;
+
 public class lessonAlfabet extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ArrayList<String>arrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_alfabet);
+
+        recyclerView = findViewById(R.id.recalfa);
+
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
