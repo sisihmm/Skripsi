7ecab1d914d70497ef054d2452c5873fbc00c36d
sisihmm <87139802+sisihmm@users.noreply.github.com>
2021-08-02 01:58:20 +0700

    k
    k

--- app/src/main/java/com/anas/skripsi/user/Pob.java
+++ app/src/main/java/com/anas/skripsi/user/Pob.java
@@ -1,22 +1,67 @@
 package com.anas.skripsi.user;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
 
 import com.anas.skripsi.R;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Pob extends AppCompatActivity {
+
+    private ScaleGestureDetector scaleGestureDetector;
+    private float mScaleFactor = 1.0f;
+    private ImageView imageView;
+    RecyclerView recyclerView;
+    ArrayList<String> pobArrayList;
+    pobAdapter pobAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pob);
 
+        recyclerView = findViewById(R.id.repob);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
 
+        pobAdapter = new pobAdapter(Pob.this,pobArrayList);
+        recyclerView.setAdapter(pobAdapter);
+
+        pobArrayList = new ArrayList<>();
+        pobArrayList.add("head ");
+        pobArrayList.add("hair ");
+
+
+
+        imageView = findViewById(R.id.ivpob);
+        scaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
-}
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        scaleGestureDetector.onTouchEvent(motionEvent);
+        return true;
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 10.0f));
+            imageView.setScaleX(mScaleFactor);
+            imageView.setScaleY(mScaleFactor);
+            return true;
+        }
+
+    }
+    }
---
+++ app/src/main/java/com/anas/skripsi/user/pobAdapter.java
@@ -0,0 +1,54 @@
+package com.anas.skripsi.user;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.anas.skripsi.R;
+
+import java.util.ArrayList;
+
+public class pobAdapter extends RecyclerView.Adapter<pobAdapter.pobViewHolder> {
+
+    Context context;
+    ArrayList<String> pobArrayList;
+
+    public pobAdapter(Context context, ArrayList<String> pobArrayList){
+        this.context = context;
+        this.pobArrayList = pobArrayList;
+    }
+
+    @NonNull
+    @Override
+    public pobAdapter.pobViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.pobcv, parent,  false);
+        return new pobViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull pobAdapter.pobViewHolder holder, int position) {
+
+        String f = pobArrayList.get(position);
+        holder.top.setText(f);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return pobArrayList.size(); }
+
+    public static class pobViewHolder extends RecyclerView.ViewHolder {
+        TextView top;
+
+        public pobViewHolder(@NonNull View itemView) {
+            super(itemView);
+            top = itemView.findViewById(R.id.pob1);
+
+        }
+    }
+}
